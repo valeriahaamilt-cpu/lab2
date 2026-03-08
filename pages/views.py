89ab36b4533ed12e34c154eab2b10e28cd3b9ac3
valeriahaamilt-cpu <valeriahaamilt@gmail.com>
2026-03-08 23:49:51 +0200

    Lab 3: add pages app with templates and navigation

---
+++ pages/views.py
@@ -0,0 +1,16 @@
+﻿from django.shortcuts import render
+
+def home(request):
+    return render(request, "pages/home.html", {"title": "Головна"})
+
+def about(request):
+    return render(request, "pages/about.html", {
+        "title": "Про нас",
+        "text": "Цей текст передано через context у render()."
+    })
+
+def contact(request):
+    return render(request, "pages/contact.html", {
+        "title": "Контакти",
+        "text": "Текст також передається через context."
+    })
